5bbcee8364eb6f5326d9d5e435a58bee1fc7b063
mozahid <ce.muzahid@gmail.com>
2024-09-14 17:35:17 +0600

    added two cad projects

--- database/dbAutoCad.js
+++ database/dbAutoCad.js
@@ -1,9 +1,24 @@
 const cadDrawing = [
   {
+    id: 1,
     title: "APD of Export Trading BD Ltd.",
     thumbnail: "https://i.postimg.cc/Gtttn4CJ/Export-Trading-BD-Ltd.png",
     pdfLink:
       "https://drive.google.com/file/d/1ed7ZAtG8wEOp6jEhAY7Gn-_IYqSLT4fa/view?usp=drive_link",
+  },
+  {
+    id: 2,
+    title: "Warehouse Building",
+    thumbnail: "https://i.postimg.cc/wvrpvmjw/Warehouse-Building.png",
+    pdfLink:
+      "https://drive.google.com/file/d/1tbVnGrY-ZIfnx4s_hjsaqAmoEzaKs0Ke/view?usp=drive_link",
+  },
+  {
+    id: 3,
+    title: "Chemical Godown Building",
+    thumbnail: "https://i.postimg.cc/SsCBytHx/03-Chemical-Godown.png",
+    pdfLink:
+      "https://drive.google.com/file/d/1MTt7RarmhS_MGyom4dVQJLGLVR9FnhL7/view?usp=drive_link",
   },
 ];
 
