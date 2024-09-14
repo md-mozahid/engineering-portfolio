d17e888f8b6e8d6ba8465645967dfd8010a58bc3
mozahid <ce.muzahid@gmail.com>
2024-09-14 13:36:28 +0600

    added cad file

--- app/autocad/page.js
+++ app/autocad/page.js
@@ -1,9 +1,36 @@
-import React from "react";
+import { getCadDrawings } from "@/database/dbAutoCad";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function AutoCAD() {
+  const cadDrawings = getCadDrawings();
+  // console.log(cadDrawings)
+
   return (
-    <div className="flex items-center justify-center">
-      AutoCAD Project upload soon...
+    <div className="container py-16">
+      <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-5 mb-5">
+        {cadDrawings?.map((pdf, i) => (
+          <div key={i} className="relative rounded-lg group h-[277px]">
+            <Image
+              src={pdf?.thumbnail}
+              alt={pdf?.title}
+              className="w-full rounded-lg object-cover object-center"
+              width={500}
+              height={500}
+            />
+            <div className="absolute inset-0 bg-black rounded-lg bg-opacity-50 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+              <Link
+                href={pdf?.pdfLink}
+                target="_blank"
+                className="text-white text-lg w-28 h-8 rounded-full bg-teal-500 flex items-center justify-center hover:scale-x-110 transition"
+                title="going to google drive"
+              >
+                <p>Preview</p>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ database/dbAutoCad.js
@@ -0,0 +1,12 @@
+const cadDrawing = [
+  {
+    title: "APD of Export Trading BD Ltd.",
+    thumbnail: "https://i.postimg.cc/Gtttn4CJ/Export-Trading-BD-Ltd.png",
+    pdfLink:
+      "https://drive.google.com/file/d/1ed7ZAtG8wEOp6jEhAY7Gn-_IYqSLT4fa/view?usp=drive_link",
+  },
+];
+
+export const getCadDrawings = () => {
+  return cadDrawing;
+};
